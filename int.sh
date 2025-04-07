6e8c5321a67c25c07d44d55f83ef63325efd251a
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-04-07 15:56:49 +0530

    Add files via upload

---
+++ int.sh
@@ -0,0 +1,17 @@
+if [ $# -ne 3 ]
+then
+echo "The syntax is <$0> <no1> <no2> <no3>"
+exit 1
+fi
+a=$1
+b=$2
+c=$3
+if [$a -gt $b -a $a -gt $c]
+then
+echo "maximum is $a"
+elif[$b -gt $a -a $b - gt $c]
+then 
+echo "maximum is $b"
+else
+echo"maximum is $c"
+fi
