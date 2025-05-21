0b2f6ee45080434e498e449c5435ccc6a08b6331
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-05-21 22:56:33 +0530

    Create lninfile.sh

---
+++ Network lab/lninfile.sh
@@ -0,0 +1,15 @@
+
+if [ $# -le 2 ]; then
+echo "Syntax: $0 <start_line> <end_line> <file>"> exit 1
+fi
+n1=$1
+n2=$2
+shift
+shift
+n3=$(expr $n2 - $n1 + 1)
+while [ $# -ne 0 ]; do
+echo "File: $1"
+head -n $n2 "$1" | tail -n $n3
+shift
+done
+exit 0
