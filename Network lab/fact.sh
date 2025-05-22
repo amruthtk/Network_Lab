99d7e32c98bff132d47de1fe8377b58691f1626a
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-05-22 11:44:45 +0530

    Update fact.sh

--- Network lab/fact.sh
+++ Network lab/fact.sh
@@ -1,32 +1,24 @@
-
 if [ $# -ne 2 ]
 then
-  echo "Syntax is <$0> <n> <r>"
+ echo "Syntax is <$0> <n> <r>"
  exit 1
 fi
-
-# Factorial function
 fact() {
   n=$1
   i=1
   p=1
-
   while [ $i -le $n ]
   do
-    p=`expr $p \* $i`
-    i=`expr $i + 1`
+ p=`expr $p \* $i`
+i=`expr $i + 1`
   done
-
-  echo $p
+echo $p
 }
-
 n=$1
 r=$2
-
 nP=$(fact $n)
 rP=$(fact $r)
 n_rP=$(fact `expr $n - $r`)
 den=`expr $rP \* $n_rP`
 nCr=`expr $nP / $den`
-
 echo "$n C $r = $nCr"
