0883ed9f7fca9362dfaa25114f8c11272174651d
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-05-22 10:54:49 +0530

    Update delln.sh

--- Network lab/delln.sh
+++ Network lab/delln.sh
@@ -1,13 +1,13 @@
-if [ $# -ne 2 ]; then
+if [ $# -ne 2 ]; then 
 echo "Syntax is $0 <file> <word to delete>"
-exit 1
-fi
-if grep -q "\b$2\b" "$1"  
-then
+ exit 1 
+fi 
+if grep -q "$2" "$1"
+then 
 echo "$2 deleted from $1 succesfully"
-grep -v "$2" $1 > temp
-mv temp $1
-else
-echo "error deleting $2 from $1"
-fi
-exit 0 
+grep -v "$2" $1 > temp 
+mv temp $1 
+else 
+echo "error deleting $2 from $1" 
+fi 
+
